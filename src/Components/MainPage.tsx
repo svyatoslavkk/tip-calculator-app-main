a6a39fc425be19956b099835e30616ae862f9195
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-12 20:04:10 +0300

    feat: ResetButton styled components

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from 'styled-components';
 import logo from '../logo.svg';
 import iconPerson from '../icon-person.svg';
 import iconDollar from '../icon-dollar.svg';
@@ -47,6 +48,23 @@ export default function MainPage () {
         setTipPercentage(0);
         setNumberOfPeople(1);
     };
+
+    const ResetButton = styled.button`
+        width: 100%;
+        text-transform: uppercase;
+        background-color: var(--primary-strong-cyan);
+        color: var(--neutral-very-dark-cyan);
+        font-weight: var(--font-weight);
+        font-size: var(--body-copy-font-size);
+        transition: 0.1s ease;
+
+        &:hover,
+        &:active,
+        &:focus {
+            color: var(--neutral-white);
+            box-shadow: 0 0px 12px rgba(255, 255, 255, 0.3);
+        }
+    `;
 
     return (
         <div className="container">
@@ -116,7 +134,7 @@ export default function MainPage () {
                             </div>
                         </div>
                     </div>
-                    <button className="reset-button" onClick={handleReset}>Reset</button>
+                    <ResetButton onClick={handleReset}>Reset</ResetButton>
                 </div>
             </div>
         </div>
